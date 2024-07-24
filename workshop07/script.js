454bb9edb6b6c6e0e0df42b4663df044c9f14825
JoeyEkapob <honhon16461@gmail.com>
2024-07-24 23:11:50 +0700

    edit format workshop07

--- workshop07/script.js
+++ workshop07/script.js
@@ -2,12 +2,12 @@ const caldisplay = document.querySelector('h1')
 const inputbtn = document.querySelectorAll('button')
 const clearbtn = document.getElementById('claerbtn')
 
-const calculate={
-    "/":(firstnum,secondnum)=>secondnum > 0 ? firstnum/secondnum : "error",
-    "*":(firstnum,secondnum)=> firstnum*secondnum,
-    "+":(firstnum,secondnum)=> firstnum+secondnum,
-    "-":(firstnum,secondnum)=> firstnum-secondnum,
-    "=":(firstnum,secondnum)=> secondnum
+const calculate = {
+    "/": (firstnum, secondnum) => secondnum > 0 ? firstnum / secondnum : "error",
+    "*": (firstnum, secondnum) => firstnum * secondnum,
+    "+": (firstnum, secondnum) => firstnum + secondnum,
+    "-": (firstnum, secondnum) => firstnum - secondnum,
+    "=": (firstnum, secondnum) => secondnum
 
 }
 
@@ -15,63 +15,76 @@ let firstvale = 0;
 let operatorvalue = '';
 let watifornext = false;
 
-function setnumbervaluse(num){
-    
-    if(watifornext){
-        caldisplay.textContent=num;
-        watifornext=false;
+function setnumbervaluse(num) {
 
-    }else{
+    if (watifornext) {
+
+        caldisplay.textContent = num;
+        watifornext = false;
+
+    } else {
         const displayvalue = caldisplay.textContent;
-        caldisplay.textContent = displayvalue === '0' ? num : displayvalue+num
+        caldisplay.textContent = displayvalue === '0' ? num : displayvalue + num
     }
-    
+
 }
-function calloperator(operator){
+function calloperator(operator) {
+    console.log(caldisplay.textContent)
     const currentvalus = Number(caldisplay.textContent)
-    if(operatorvalue && watifornext){
-        operatorvalue=operator;
+
+
+    if (operatorvalue && watifornext) {
+        operatorvalue = operator;
+  
         return;
     }
-    if(!firstvale){
-        firstvale=currentvalus;
-    }else{
-        const result =calculate[operatorvalue](firstvale,currentvalus);
-        caldisplay.textContent=result;
-        firstvale=result;
-        if(firstvale === "error"){
+    console.log('1')
+    if (!firstvale) {
+    
+
+        firstvale = currentvalus;
+        console.log(firstvale)
+    } else {
+        console.log(operatorvalue);
+        console.log(firstvale);
+        console.log(currentvalus);
+        const result = calculate[operatorvalue](firstvale, currentvalus);
+
+        caldisplay.textContent = result;
+        firstvale = result;
+        if (firstvale === "error") {
             resetall();
         }
     }
     operatorvalue = operator;
-    watifornext=true;
+    watifornext = true;
 
 
 }
-function adddecimal(){
-    if(watifornext) return
-    if (!caldisplay.textContent.includes(".")){
-    caldisplay.textContent=`${caldisplay.textContent}.`;
+function adddecimal() {
+    if (watifornext) return
+    if (!caldisplay.textContent.includes(".")) {
+        caldisplay.textContent = `${caldisplay.textContent}.`;
     }
-    
+
 }
 
-inputbtn.forEach((input)=>{
-    if(input.classList.length === 0 ){
-        input.addEventListener('click',()=>setnumbervaluse(input.value))
-    }else if(input.classList.contains("operator")){
-        input.addEventListener('click',()=>calloperator(input.value))
+inputbtn.forEach((input) => {
+    if (input.classList.length === 0) {
+        input.addEventListener('click', () => setnumbervaluse(input.value))
+    } else if (input.classList.contains("operator")) {
+        input.addEventListener('click', () => calloperator(input.value))
 
-    }else if(input.classList.contains("decimal")){
-        input.addEventListener('click',()=>adddecimal());
+    } else if (input.classList.contains("decimal")) {
+        input.addEventListener('click', () => adddecimal());
     }
 })
-function resetall(){
-    firstvale=0;
-    operatorvalue='';
-    watifornext=false;
-    caldisplay.textContent='0';
-  
+function resetall() {
+    firstvale = 0;
+    operatorvalue = '';
+    watifornext = false;
+    caldisplay.textContent = '0';
+
 }
 
-clearbtn.addEventListener('click' ,()=>resetall())
+clearbtn.addEventListener('click', () => resetall())
