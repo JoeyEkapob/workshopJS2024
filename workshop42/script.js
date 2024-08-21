2dc986c85db54fd28f0d594985ff003ca2170dcf
JoeyEkapob <honhon16461@gmail.com>
2024-08-21 23:04:09 +0700

    workshop 42 success 20% not success

---
+++ workshop42/script.js
@@ -0,0 +1,15 @@
+let = tools =['html','css3','javascript']
+/* console.log(tools[1]) */
+/* tools.forEach((e,index) => {
+
+    console.log(index,e)
+    
+}); */
+
+/* let result = "ภาษาเขียนโปรเเกรม = " + tools.join(' ')
+console.log(result) */
+
+let url=['www','example',"com"]
+let result  =  url.join('.')
+console.log(result) 
+
