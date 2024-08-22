3e16f2ccf863df191b168529e487dc1e6b5e3cfa
JoeyEkapob <honhon16461@gmail.com>
2024-08-22 17:20:30 +0700

    workshop43 success 30%

---
+++ workshop43/script.js
@@ -0,0 +1,18 @@
+let text='jkeyekapob'
+/* let text2='sesantae'
+let result = text + text2
+let result2 = text.concat(text2)
+console.log(result)
+console.log(result2) */
+/* console.log(text[0]) */
+
+/*  
+text.indexOf('k')
+console.log(text.indexOf('k'))
+console.log(text.lastIndexOf('k'))*/
+
+
+let joey =text.substring(0,3)
+console.log(text.substring(3))
+console.log(joey)
+
