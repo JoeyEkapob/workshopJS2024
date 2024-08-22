b7281b49c61f894a8bb7137477386052eb48d603
JoeyEkapob <honhon16461@gmail.com>
2024-08-22 16:01:59 +0700

    workshop42 seccuss

--- workshop42/script.js
+++ workshop42/script.js
@@ -1,4 +1,4 @@
-let = tools =['html','css3','javascript']
+let = tools =['html','css3','javascript','node.js','angular']
 /* console.log(tools[1]) */
 /* tools.forEach((e,index) => {
 
@@ -9,7 +9,42 @@ let = tools =['html','css3','javascript']
 /* let result = "ภาษาเขียนโปรเเกรม = " + tools.join(' ')
 console.log(result) */
 
-let url=['www','example',"com"]
+/*  let url=['www','example',"com"]
 let result  =  url.join('.')
-console.log(result) 
+console.log(result)  */
 
+
+
+/* console.log(tools.indexOf('css3'))   
+console.log(tools.lastIndexOf('css3'))  */
+
+/* tools.sort(); */
+/* tools.reverse();
+console.log(tools)   */
+
+
+/* console.log(tools.includes("css3"))  
+console.log(tools.includes("css3"))  */ 
+
+/* let newtools = ['react','vue']
+let web = tools.concat(newtools)
+ web.fill('php',0,3) 
+console.log(web) */
+
+/* tools.push('java')
+tools.push('react')
+tools.pop()
+console.log(tools) */
+
+/* tools.unshift("java")
+tools.unshift('react')
+tools.shift()
+
+console.log(tools)
+ */
+
+/* let copytools = tools.slice(3) */
+let copytools = tools.splice(3)
+
+console.log(copytools)
+console.log(tools)
