2e3d765aed6bf1cb17f2c91a731e920f36a8c0d1
JoeyEkapob <honhon16461@gmail.com>
2024-08-27 22:43:43 +0700

    workshop47 25%

--- workshop47/script.js
+++ workshop47/script.js
@@ -1,10 +1,23 @@
-const canvas = document.getElementById('canvas')
- const context = canvas.getContext("2d") 
- context.moveTo(40,40)
- context.lineTo(240,40)
- context.lineTo(240,240)
- context.lineTo(240,240)
- 
- context.lineWidth=3
- context.strokeStyle="red"
- context.stroke()
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+
+/* context. */
+
+/* context.fillStyle="green"
+context.fillRect(10,10,50,50) */
+/* context.strokeStyle="green"
+context.strokeRect(10,10,50,50) */
+/* context.beginPath()
+context.moveTo(40, 40);
+context.lineTo(240, 40);
+context.lineTo(240, 240);
+context.lineTo(40, 240);
+context.closePath()
+context.lineWidth = 5;
+context.strokeStyle = "red";
+context.stroke();
+
+context.fillStyle="yellow"
+context.fill()
+ */
