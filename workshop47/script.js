75636103e9540a266113dd3c81173d192040b7d0
JoeyEkapob <honhon16461@gmail.com>
2024-08-28 17:26:11 +0700

    workshop47 50%

--- workshop47/script.js
+++ workshop47/script.js
@@ -1,6 +1,50 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
+let board ;
+let boardwidth=800;
+let boardheight=300;
+let context;
 
+let playerwidth = 64;
+let playerheight = 64;
+let playerX = 50;
+let playerY = boardheight-playerheight
+let playerimg
+let player={
+    x:playerX,
+    y:playerY,
+    width:playerwidth,
+    height:playerheight
+}
+
+
+window.onload=function(){
+    
+    board = document.getElementById('borad')
+     board.height=boardheight
+    board.width=boardwidth
+    context=board.getContext('2d')
+    playerimg=new Image()
+    playerimg.src="./img/player.png"
+    playerimg.onload=function(){
+        
+            console.log(playerimg)
+            context.drawImage(playerimg,player.x,player.y,player.width,player.height)
+        
+    }
+}
+
+/* const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const img = new Image()
+img.src="./img/player.png"
+img.onload=function(){
+    context.drawImage(img,10,20,100,100)
+} */
+/* context.font="bold italic 50px Arial"
+context.fillStyle="red"
+context.fillText('ekapob',50,75)
+context.strokeStyle="blue"
+context.strokeText('joey',50,120)
+ */
 
 /* context. */
 
