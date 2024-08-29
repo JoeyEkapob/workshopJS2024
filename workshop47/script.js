f3dcd5ebd275f8c446cf499e769353f52883b211
JoeyEkapob <honhon16461@gmail.com>
2024-08-29 11:11:20 +0700

    workshop 47 success

--- workshop47/script.js
+++ workshop47/script.js
@@ -1,54 +1,128 @@
-let board ;
-let boardwidth=800;
-let boardheight=300;
+let board;
+let boardwidth = 800;
+let boardheight = 300;
 let context;
 
 let playerwidth = 64;
 let playerheight = 64;
 let playerX = 50;
-let playerY = boardheight-playerheight
+let playerY = boardheight - playerheight
 let playerimg
-let player={
-    x:playerX,
-    y:playerY,
-    width:playerwidth,
-    height:playerheight
+let player = {
+    x: playerX,
+    y: playerY,
+    width: playerwidth,
+    height: playerheight
 }
-let velocity=0;
-let gravity=0.25       ;
-window.onload=function(){
-    
+let velocity = 0;
+let gravity = 0.25;
+
+let treeimg;
+let treewidth=70;
+let treeheight=90;
+let treeX=700;
+let treeY=boardheight-treeheight;
+
+let treearray=[];
+let treespeed=-3;
+let score = 0;
+let gameover=false
+
+window.onload = function () {
+
     board = document.getElementById('borad')
-    board.height=boardheight
-    board.width=boardwidth
-    context=board.getContext('2d')
-    playerimg=new Image()
-    playerimg.src="./img/player.png"
-    playerimg.onload=function(){
-        
-            
-            context.drawImage(playerimg,player.x,player.y,player.width,player.height)
-        
+    board.height = boardheight
+    board.width = boardwidth
+    context = board.getContext('2d')
+    playerimg = new Image()
+    playerimg.src = "./img/player.png"
+    playerimg.onload = function () {
+
+
+        context.drawImage(playerimg, player.x, player.y, player.width, player.height)
+
+
     }
+    treeimg=new Image();
+    treeimg.src="./img/tree.png"
     requestAnimationFrame(update)
-    document.addEventListener('keydown',movePlayer)
+    setInterval(createtree,3000)
+    document.addEventListener('keydown', movePlayer)
 
 }
 
-function update(){
+function update() {
     requestAnimationFrame(update)
-    context.clearRect(0,0,board.width,board.height)
-    velocity+=gravity
-    player.y= Math.min(player.y+velocity,playerY)/* Math.min(player.y+velocity,playerY) */
-    context.drawImage(playerimg,player.x,player.y,player.width,player.height)
+    if(gameover){
+        return
+    }
+    context.clearRect(0, 0, board.width, board.height)
+    velocity += gravity
+    // create player
 
-}
-
-function movePlayer(e){
-    if(e.code==="Space" && player.y == playerY ){
+    player.y = Math.min(player.y + velocity, playerY)/* Math.min(player.y+velocity,playerY) */
+    context.drawImage(playerimg, player.x, player.y, player.width, player.height)
+    // create tree 
+    for(let i=0;i<treearray.length;i++){
+        let tree = treearray[i]
+        tree.x+=treespeed
+        context.drawImage(tree.img,tree.x,tree.y,tree.width,tree.height)
+    if(oncollision(player,tree)){
+        gameover=true
+        context.font="normal bold 40px Arial"
        
-        velocity=-10
+        context.textAlign="center"
+        context.fillStyle="red"
+        context.fillText("Game Over",boardwidth/2,boardheight/2)
+        
     }
+    }
+
+    score++
+    context.font="normal bold 15px Arial"
+    context.textAlign="left"
+    context.fillStyle="red"
+    context.fillText(score++,755,20)
+}
+
+function movePlayer(e) {
+    if(gameover){
+        return
+    }
+    if (e.code === "Space" && player.y == playerY) {
+
+        velocity = -10
+    }
+}
+function createtree(){
+    if(gameover){
+        return
+    }
+    let tree={
+        img:treeimg,
+        x:treeX,
+        y:treeY,
+        width:treewidth,
+        height:treeheight
+    }
+    treearray.push(tree)
+    if(treearray.length>5){
+        treearray.shift()
+        
+    }
+    /* console.log(treearray) */
+    // display
+   
+    
+}
+
+function oncollision(obj1,obj2){
+    return obj1.x<obj2.x+obj2.width && obj1.x+obj1.width>obj2.x 
+    && obj1.y<obj2.y+obj2.height && obj1.y+obj1.height>obj2.y
+
+}
+function restart(){
+    location.reload()
 }
 /* const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
